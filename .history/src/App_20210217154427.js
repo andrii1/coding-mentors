045d1d0f3f3d1c1ef8835833ab74481e71dc55fb
Andrii <andrii.gorokhovskyi@gmail.com>
2021-02-17 16:28:55 +0100

    update

--- .history/src/App_20210217154427.js
+++ .history/src/App_20210217154427.js
@@ -139,7 +139,6 @@ export default function BasicExample() {
         </h1>
         <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
           Find a coding mentor or become one.
-          <Mentors />
         </p>
         <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
           <div className="rounded-md shadow">
--- .history/src/App_20210217154606.js
+++ .history/src/App_20210217154606.js
@@ -139,7 +139,6 @@ export default function BasicExample() {
         </h1>
         <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
           Find a coding mentor or become one.
-          <Mentors />
         </p>
         <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
           <div className="rounded-md shadow">
@@ -149,7 +148,7 @@ export default function BasicExample() {
           </div>
           <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
             <a href="#" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10">
-              Find mentors
+              Find mentors<Mentors />
             </a>
           </div>
         </div>
